9b72464083df247cc760dba4b27ddb2ed5fc2850
Matas Girdauskas <matas.girdauskas@lightcon.com>
2021-03-28 15:02:54 +0300

    Working Shelly1pmClient.

---
+++ ShellieToHomie/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Receiving;
+
+namespace ShellieToHomie {
+    class Program {
+        private static IMqttClient _mqttClient;
+        private static Shelly1pmClient _shelly1PmClient = new Shelly1pmClient();
+
+        static async Task Main(string[] args) {
+            var mqttFactory = new MqttFactory();
+            _mqttClient = mqttFactory.CreateMqttClient();
+            _mqttClient.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(HandleMessage);
+            var clientOptions = new MqttClientOptions { ChannelOptions = new MqttClientWebSocketOptions { Uri = "ws://192.168.2.2:9001/" } };
+
+            await _mqttClient.ConnectAsync(clientOptions, CancellationToken.None);
+
+            _shelly1PmClient.Initialize("shellies/shelly1pm-68C63AFADFF9", PublishMqttTopic);
+            await _mqttClient.SubscribeAsync("shellies/shelly1pm-68C63AFADFF9/#");
+
+            while (true) {
+                Console.WriteLine("Hello World!");
+
+                if (_shelly1PmClient.RelayState) await _shelly1PmClient.DisableRelayAsync();
+                else await _shelly1PmClient.EnableRelayAsync();
+
+                await Task.Delay(3000);
+            }
+        }
+
+        private static async Task PublishMqttTopic(string topic, string value) {
+            await _mqttClient.PublishAsync(topic, Encoding.UTF8.GetBytes(value));
+        }
+
+        private static void HandleMessage(MqttApplicationMessageReceivedEventArgs obj) {
+            _shelly1PmClient.ProcessMqttMessage(obj.ApplicationMessage.Topic, Encoding.UTF8.GetString(obj.ApplicationMessage.Payload));
+        }
+    }
+}
---
+++ ShellieToHomie/Shelly1pmClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace ShellieToHomie {
+    public class Shelly1pmClient {
+        private Func<string, string, Task> _publishMqttTopic;
+
+        public bool IsInitialized { get; private set; }
+        public string RootMqttTopic { get; private set; }
+        public bool RelayState { get; private set; }
+        public double RelayPower { get; private set; }
+        public int RelayEnergy { get; private set; }
+        public bool ButtonState { get; private set; }
+        public double Temperature { get; private set; }
+
+        public async Task EnableRelayAsync() {
+            if (IsInitialized == false) throw new InvalidOperationException("Object must be initialized before commands can be executed.");
+            await _publishMqttTopic($"{RootMqttTopic}/relay/0/command", "on");
+        }
+
+        public async Task DisableRelayAsync() {
+            if (IsInitialized == false) throw new InvalidOperationException("Object must be initialized before commands can be executed.");
+            await _publishMqttTopic($"{RootMqttTopic}/relay/0/command", "off");
+        }
+
+        public void ProcessMqttMessage(string topic, string value) {
+            if (IsInitialized == false) throw new InvalidOperationException("Object must be initialized before messages can be processed.");
+
+            if (topic == $"{RootMqttTopic}/relay/0") RelayState = value == "on";
+            else if (topic == $"{RootMqttTopic}/relay/0/power") RelayPower = double.Parse(value, CultureInfo.InvariantCulture);
+            else if (topic == $"{RootMqttTopic}/relay/0/energy") RelayEnergy = int.Parse(value, CultureInfo.InvariantCulture);
+            else if (topic == $"{RootMqttTopic}/input/0") ButtonState = value == "1";
+            else if (topic == $"{RootMqttTopic}/temperature") Temperature = double.Parse(value, CultureInfo.InvariantCulture);
+        }
+
+        public void Initialize(string rootMqttTopic, Func<string, string, Task> publishMqttTopic) {
+            if (IsInitialized) throw new InvalidOperationException("Object can only be initialized once.");
+
+            _publishMqttTopic = publishMqttTopic;
+            RootMqttTopic = rootMqttTopic;
+            IsInitialized = true;
+        }
+    }
+}
